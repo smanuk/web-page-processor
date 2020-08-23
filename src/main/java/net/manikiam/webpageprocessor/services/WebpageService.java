56b2757d752c919d41b3fd8491a8708731add04b
Steven Manikiam <smanikiam@videotel.com>
2020-08-23 11:12:55 +0100

    Stub of web page service, we will use this service to load a web page.

---
+++ src/main/java/net/manikiam/webpageprocessor/services/WebpageService.java
@@ -0,0 +1,19 @@
+package net.manikiam.webpageprocessor.services;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * @author smanikiam
+ */
+public class WebpageService {
+
+    public static WebpageService getNewInstance() {
+
+        return new WebpageService();
+    }
+
+    private WebpageService() {
+
+    }
+}
