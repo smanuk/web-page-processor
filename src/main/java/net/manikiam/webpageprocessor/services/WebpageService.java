26f6f8b1c15565da9526bd457dbfd2b3f54d9d58
Steven Manikiam <smanikiam@videotel.com>
2020-08-23 11:27:04 +0100

    service and test for getting web pages.

--- src/main/java/net/manikiam/webpageprocessor/services/WebpageService.java
+++ src/main/java/net/manikiam/webpageprocessor/services/WebpageService.java
@@ -16,4 +16,10 @@ public class WebpageService {
     private WebpageService() {
 
     }
+
+    public Document openWebPage(String webUrlString) throws Exception {
+
+            return Jsoup.connect(webUrlString)
+                    .get();
+    }
 }
