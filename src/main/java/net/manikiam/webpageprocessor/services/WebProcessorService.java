2f155fa45699b4520d2ce9aa346d648a50412187
Steven Manikiam <smanikiam@videotel.com>
2020-08-23 11:34:43 +0100

    Web processer stub class, this is the main coordinator class.

---
+++ src/main/java/net/manikiam/webpageprocessor/services/WebProcessorService.java
@@ -0,0 +1,15 @@
+package net.manikiam.webpageprocessor.services;
+
+/**
+ * @author smanikiam
+ */
+public class WebProcessorService {
+    
+    public static WebProcessorService getNewInstance() {
+        return new WebProcessorService();
+    }
+
+    private WebProcessorService() {
+
+    }
+}
