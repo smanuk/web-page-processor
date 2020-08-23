0688f8760caecb920fb2903b2efb9c09ce108ebe
Steven Manikiam <smanikiam@videotel.com>
2020-08-23 12:38:48 +0100

    Script parse service stub

---
+++ src/main/java/net/manikiam/webpageprocessor/services/ScriptParserService.java
@@ -0,0 +1,15 @@
+package net.manikiam.webpageprocessor.services;
+
+/**
+ * @author smanikiam
+ */
+public class ScriptParserService {
+
+    public static ScriptParserService getNewInstance() {
+        return new ScriptParserService();
+    }
+
+    private ScriptParserService() {
+
+    }
+}
