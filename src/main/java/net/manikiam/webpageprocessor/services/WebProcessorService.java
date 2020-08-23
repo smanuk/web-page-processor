585fa7d58f7dc8610a936324b0e5969eeacf38e2
Steven Manikiam <smanikiam@videotel.com>
2020-08-23 11:43:16 +0100

    Web processor service inclusion of coordinator method countScripts.

--- src/main/java/net/manikiam/webpageprocessor/services/WebProcessorService.java
+++ src/main/java/net/manikiam/webpageprocessor/services/WebProcessorService.java
@@ -1,16 +1,32 @@
 package net.manikiam.webpageprocessor.services;
 
+import org.jsoup.nodes.Document;
+
 /**
  * @author smanikiam
  */
 public class WebProcessorService {
-    
+
+    private static String BING_SEARCH_URL = "https://www.bing.com/search?q=";
+
     public static WebProcessorService getNewInstance() {
         return new WebProcessorService();
     }
 
-    private WebProcessorService() {
 
+    private WebpageService webPageService;
+
+
+    private WebProcessorService() {
+        webPageService = WebpageService.getNewInstance();
+    }
+
+
+    public void countScripts(String searchPhrase) throws Exception {
+
+        Document webDocument = searchBing(searchPhrase);
+
+        System.out.println(webDocument);
     }
 
 
